4ae46736ed6f03e2927853eeae8384dc0b3a524d
Barbara Barreto <bbarretobarros@gmail.com>
2022-07-04 17:41:54 -0300

    Final

--- src/nova-transferencia.component.ts
+++ src/nova-transferencia.component.ts
@@ -1,20 +1,41 @@
-import { Component } from "@angular/core";
+import { TransferenciaService } from './app/services/transferencia.service';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { Transferencia } from './models/transferencia.model';
+import { Router } from '@angular/router';
 
 @Component({
-    selector:'app-nova-transferencia',
-    templateUrl:'./nova-transferencia.component.html',
-    styleUrls:['./nova-transferencia.component.scss']
-
+  selector: 'app-nova-transferencia',
+  templateUrl: './nova-transferencia.component.html',
+  styleUrls: ['./nova-transferencia.component.scss'],
 })
-export class NovaTransferenciaComponent{
+export class NovaTransferenciaComponent {
+  @Output() aoTransferir = new EventEmitter<any>();
 
   valor: number;
   destino: number;
 
-  transferir(){
-    console.log('Solicitada nova transferencia');
-    console.log('Valor: ', this.valor);
-    console.log('Destino: ', this.destino);
+  constructor(private service: TransferenciaService, private router: Router){}
+
+  transferir() {
+    console.log('Solicitada nova transferência');
+
+    const valorEmitir: Transferencia = {
+      valor: this.valor,
+      destino: this.destino,
+    };
+
+    this.service.adicionar(valorEmitir).subscribe(
+      (resultado) => {
+        console.log(resultado)
+        this.limparCampos();
+        this.router.navigateByUrl('extrato')
+      },
+      (error) => console.error(error)
+    );
   }
 
+  limparCampos(){
+    this.valor = 0;
+    this.destino = 0;
+  }
 }
